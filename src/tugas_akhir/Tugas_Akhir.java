57ecffe89662f27e8eaf1d697c703904ea1c7297
KEVIN <KEVIN@LAPTOP-SLJFLG4J>
2021-06-16 19:36:38 +0700

    tes again

--- src/tugas_akhir/Tugas_Akhir.java
+++ src/tugas_akhir/Tugas_Akhir.java
@@ -15,7 +15,7 @@ public class Tugas_Akhir {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("mpssh");
+        System.out.println("mpsshaasas");
     }
     
 }
